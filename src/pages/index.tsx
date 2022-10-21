7d36d4a2862e91a3ae152d102dda7f85f7b0a6c8
Ezra Berendsen <ezra@webbers.com>
2022-10-21 12:40:52 +0200

    Add tailwindcss.

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from "next"
+
+const HomePage: NextPage = () => {
+    return (
+        <h1 className="text-7xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
+export default HomePage
